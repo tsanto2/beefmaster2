8f68884838794964808b40b847fbc0f98b4fb508
tsanto2 <thomas.santopadre@pixeldashstudios.com>
2025-10-04 09:39:20 -0500

    Chunkbert.

---
+++ Assets/ThirdParty/PSXEffects/Example/Scripts/CameraOrbit.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraOrbit : MonoBehaviour {
+
+	public Transform origin;
+	public float speed = 20;
+
+	public bool scrollControl = false;
+	public float scrollSpeed = 10;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//transform.LookAt(origin);
+		if(scrollControl)
+			transform.Translate(Vector3.forward * Input.GetAxis("Mouse ScrollWheel") * scrollSpeed);
+
+		if (Input.GetMouseButton(0))
+		{
+			transform.RotateAround(origin.position, origin.transform.up, Time.deltaTime * speed);
+		}
+		if (Input.GetMouseButton(1))
+		{
+			transform.RotateAround(origin.position, origin.transform.up, Time.deltaTime * -speed);
+		}
+	}
+}
